a21e1c683be5ab87f055548f8b3901486edb5612
Mike Sargsyan <misargsy@c2r4s4.42tokyo.jp>
2024-05-06 21:55:16 +0900

    minor fix

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -6,13 +6,13 @@
 /*   By: misargsy <misargsy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 22:26:23 by misargsy          #+#    #+#             */
-/*   Updated: 2024/05/02 22:33:11 by misargsy         ###   ########.fr       */
+/*   Updated: 2024/05/06 20:54:53 by misargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool	bsp(Point const a, Point const b, Point const c, Point const point) {
+bool	bsp(const Point a, const Point b, const Point c, const Point point) {
 	Fixed	abc = (a.getX() - point.getX()) * (b.getY() - a.getY()) - (a.getY() - point.getY()) * (b.getX() - a.getX());
 	Fixed	bca = (b.getX() - point.getX()) * (c.getY() - b.getY()) - (b.getY() - point.getY()) * (c.getX() - b.getX());
 	Fixed	cab = (c.getX() - point.getX()) * (a.getY() - c.getY()) - (c.getY() - point.getY()) * (a.getX() - c.getX());
--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: misargsy <misargsy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 22:31:52 by misargsy          #+#    #+#             */
-/*   Updated: 2024/05/02 22:34:30 by misargsy         ###   ########.fr       */
+/*   Updated: 2024/05/06 20:56:29 by misargsy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int main(void) {
 	Point p4(4, 4);
 	Point p5(5, 5);
 
-	std::cerr << "p1 is inside the triangle: " << bsp(a, b, c, p1) << std::endl;
-	std::cerr << "p2 is inside the triangle: " << bsp(a, b, c, p2) << std::endl;
-	std::cerr << "p3 is inside the triangle: " << bsp(a, b, c, p3) << std::endl;
-	std::cerr << "p4 is inside the triangle: " << bsp(a, b, c, p4) << std::endl;
-	std::cerr << "p5 is inside the triangle: " << bsp(a, b, c, p5) << std::endl;
+	std::cerr << "p1 is inside the triangle: " << (bsp(a, b, c, p1) ? "true" : "false") << std::endl;
+	std::cerr << "p2 is inside the triangle: " << (bsp(a, b, c, p2) ? "true" : "false") << std::endl;
+	std::cerr << "p3 is inside the triangle: " << (bsp(a, b, c, p3) ? "true" : "false") << std::endl;
+	std::cerr << "p4 is inside the triangle: " << (bsp(a, b, c, p4) ? "true" : "false") << std::endl;
+	std::cerr << "p5 is inside the triangle: " << (bsp(a, b, c, p5) ? "true" : "false") << std::endl;
 
 	return 0;
 }
